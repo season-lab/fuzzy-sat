626a5b3208712ebad63c5bfc29f0b534b586c09a
Luca Borzacchiello <lucaborza@gmail.com>
2020-04-27 17:29:09 +0200

    fixed user_files header

--- user_files/z3-fuzzy.h
+++ user_files/z3-fuzzy.h
@@ -11,6 +11,7 @@ typedef struct fuzzy_stats_t {
     unsigned long input_to_state;
     unsigned long input_to_state_ext;
     unsigned long brute_force;
+    unsigned long range_brute_force;
     unsigned long gradient_descend;
     unsigned long flip1;
     unsigned long flip2;
@@ -41,6 +42,7 @@ typedef struct fuzzy_stats_t {
     unsigned long multigoal;
     unsigned long sat_in_seed;
     unsigned long num_univocally_defined;
+    unsigned long num_range_constraints;
     unsigned long num_conflicting;
     unsigned long conflicting_fallbacks;
     unsigned long conflicting_fallbacks_same_inputs;
@@ -67,6 +69,7 @@ typedef struct fuzzy_ctx_t {
     void* ast_info_cache;
     void* processed_constraints;
     void* conflicting_asts;
+    void* group_intervals;
 } fuzzy_ctx_t;
 
 void z3fuzz_init(fuzzy_ctx_t* fctx, Z3_context ctx, char* seed_filename,
